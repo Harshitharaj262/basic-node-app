e14f6a4a62c74058f559ddbe6e2cd54be8d4f2c5
harshitha2602 <harshitha@oneorigin.us>
2024-06-29 13:41:37 +0400

    new build

--- public/js/flash.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-    document.querySelector('#flash-messages').remove()
-}, 3000)
--- utils/file.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const deleteFile = (filepath)=>{
-    fs.unlink(filepath, (err)=>{
-        if(err) throw err
-    })
-}
-
-exports.deleteFile = deleteFile
